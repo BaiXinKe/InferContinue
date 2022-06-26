1aa410f291f9fd6595b9f7e666886a7e80425152
BaiXinKe <baixinke@qq.com>
2022-06-26 11:20:21 +0800

    third part dependence added

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/baixinke/InferContinue
 
 go 1.18
+
+require (
+	github.com/learning-go-book/formatter v0.0.0-20200921021027-5abc380940ae // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	crand "crypto/rand"
+	"encoding/binary"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
 
-	print "github.com/baixinke/InferContinue/formatter"
-	"github.com/baixinke/InferContinue/math"
+	"github.com/learning-go-book/formatter"
+	"github.com/shopspring/decimal"
 )
 
+func seedRand() *rand.Rand {
+	var b [8]byte
+	_, err := crand.Read(b[:])
+	if err != nil {
+		panic("cannot seed with cryptographic random number generator")
+	}
+	r := rand.New(rand.NewSource(int64(binary.LittleEndian.Uint64(b[:]))))
+	return r
+}
+
 func main() {
-	num := math.Double(2)
-	output := print.Format(num)
-	fmt.Println(output)
+	if len(os.Args) < 3 {
+		fmt.Println("Need two parameters: amount and percent")
+		os.Exit(1)
+	}
+
+	amount, err := decimal.NewFromString(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	percent, err := decimal.NewFromString(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	percent = percent.Div(decimal.NewFromInt(100))
+	total := amount.Add(amount.Mul(percent)).Round(2)
+	fmt.Println(formatter.Space(80, os.Args[1], os.Args[2], total.StringFixed(2)))
 }
